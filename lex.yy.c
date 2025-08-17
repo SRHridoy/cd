3215ce3485f8d82725053b21f8f7bdf223a304f3
Hridoy-NerdCoders <hridoy@nerdcoders.xyz>
2025-08-17 10:57:57 +0600

    Add lexical analyzer for counting vowels and consonants

--- lex.yy.c
+++ lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 4
-#define YY_END_OF_BUFFER 5
+#define YY_NUM_RULES 5
+#define YY_END_OF_BUFFER 6
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,9 +360,9 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[10] =
+static const flex_int16_t yy_accept[11] =
     {   0,
-        0,    0,    5,    3,    2,    1,    2,    3,    0
+        0,    0,    6,    4,    1,    1,    2,    3,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -370,17 +370,17 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    4,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    2,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    4,    5,    5,    5,    4,    5,
+        5,    5,    4,    5,    5,    5,    5,    5,    4,    5,
+        5,    5,    5,    5,    4,    5,    5,    5,    5,    5,
+        1,    1,    1,    1,    1,    1,    4,    5,    5,    5,
 
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        4,    5,    5,    5,    4,    5,    5,    5,    5,    5,
+        4,    5,    5,    5,    5,    5,    4,    5,    5,    5,
+        5,    5,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -397,31 +397,33 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[5] =
+static const YY_CHAR yy_meta[6] =
     {   0,
-        1,    2,    2,    1
+        1,    2,    2,    1,    1
     } ;
 
-static const flex_int16_t yy_base[11] =
+static const flex_int16_t yy_base[12] =
     {   0,
-        0,    0,    6,    0,    7,    7,    0,    0,    7,    4
+        0,    0,    7,    8,    0,    0,    8,    8,    0,    8,
+        4
     } ;
 
-static const flex_int16_t yy_def[11] =
+static const flex_int16_t yy_def[12] =
     {   0,
-        9,    1,    9,   10,    9,    9,   10,   10,    0,    9
+       10,    1,   10,   10,   11,   11,   10,   10,   11,    0,
+       10
     } ;
 
-static const flex_int16_t yy_nxt[12] =
+static const flex_int16_t yy_nxt[14] =
     {   0,
-        4,    5,    6,    7,    8,    9,    3,    9,    9,    9,
-        9
+        4,    5,    6,    7,    8,    9,   10,    3,   10,   10,
+       10,   10,   10
     } ;
 
-static const flex_int16_t yy_chk[12] =
+static const flex_int16_t yy_chk[14] =
     {   0,
-        1,    1,    1,    1,   10,    3,    9,    9,    9,    9,
-        9
+        1,    1,    1,    1,    1,   11,    3,   10,   10,   10,
+       10,   10,   10
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -438,12 +440,12 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "05_count_no_of_lines_words_spaces_chars.l"
-#line 2 "05_count_no_of_lines_words_spaces_chars.l"
-#include<stdio.h>
-int lineCnt = 0, spaceCnt = 0, wordCnt = 0, charCnt = 0;
-#line 446 "lex.yy.c"
-#line 447 "lex.yy.c"
+#line 1 "01_practice.l"
+#line 2 "01_practice.l"
+#include<stdio.h>  
+int vowelCnt = 0, consonantCnt = 0;  
+#line 448 "lex.yy.c"
+#line 449 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -660,9 +662,9 @@ YY_DECL
 		}
 
 	{
-#line 6 "05_count_no_of_lines_words_spaces_chars.l"
+#line 6 "01_practice.l"
 
-#line 666 "lex.yy.c"
+#line 668 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -689,13 +691,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 10 )
+				if ( yy_current_state >= 11 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 7 );
+		while ( yy_base[yy_current_state] != 8 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -722,25 +724,30 @@ do_action:	/* This label is used only to access EOF actions. */
 case 1:
 /* rule 1 can match eol */
 YY_RULE_SETUP
-#line 7 "05_count_no_of_lines_words_spaces_chars.l"
-{lineCnt++; charCnt += yyleng;}
+#line 7 "01_practice.l"
+{/*ignore*/}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 8 "05_count_no_of_lines_words_spaces_chars.l"
-{spaceCnt++; charCnt += yyleng;}
+#line 8 "01_practice.l"
+{vowelCnt++;}
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 9 "05_count_no_of_lines_words_spaces_chars.l"
-{wordCnt++; charCnt += yyleng;}
+#line 9 "01_practice.l"
+{if (!(yytext[0]=='a'||yytext[0]=='e'||yytext[0]=='i'||yytext[0]=='o'||yytext[0]=='u'||yytext[0]=='A'||yytext[0]=='E'||yytext[0]=='I'||yytext[0]=='O'||yytext[0]=='U')) consonantCnt++;}
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 10 "05_count_no_of_lines_words_spaces_chars.l"
+#line 10 "01_practice.l"
+{/*ignore special symbols*/}
+	YY_BREAK
+case 5:
+YY_RULE_SETUP
+#line 11 "01_practice.l"
 ECHO;
 	YY_BREAK
-#line 744 "lex.yy.c"
+#line 751 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1037,7 +1044,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 10 )
+			if ( yy_current_state >= 11 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1065,11 +1072,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 10 )
+		if ( yy_current_state >= 11 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 9);
+	yy_is_jam = (yy_current_state == 10);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1745,16 +1752,14 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 10 "05_count_no_of_lines_words_spaces_chars.l"
+#line 11 "01_practice.l"
 
 
 int main(){
-    printf("Enter the string: ");
+    printf("Enter the input: ");
     yylex();
-
-    printf("\nNo. of Words are : %d\n", wordCnt);
-    printf("No. of Spaces are : %d\n", spaceCnt);
-    printf("No. of Lines are : %d\n", lineCnt);
+    printf("No. of Vowels : %d\n",vowelCnt);
+    printf("No. of Consonants : %d\n",consonantCnt);
 }
 
 int yywrap(){
